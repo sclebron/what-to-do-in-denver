b978ad9ab2265245177aab08152f643424c58600
SLeBron <sophieclebron@gmail.com>
2023-06-07 21:21:56 -0600

    deleted denver image

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './homepage.css';
 import rmnp from './images/rmnp.png';
-import denver from './images/denver.png';
 
 function Homepage() {
     return (
         <div className="homeContainer">
             <div className="header">What To Do In Denver</div>
             <div className="underHeader">
-                <img src={denver} alt="denver" className="backgroundImg" />
+                <img src={rmnp} alt="denver" className="backgroundImg" />
                 <button className="questionnaireBtn">Start the questionnaire</button>
             </div>
         </div>

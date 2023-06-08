910962417bd9d9e80f241752d8222f37473c7272
SLeBron <sophieclebron@gmail.com>
2023-06-07 21:09:31 -0600

    got rid of some image options

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './homepage.css';
-import denver from './images/denver.png';
+import rmnp from './images/rmnp.png';
 
 function Homepage() {
     return (
         <div className="homeContainer">
             <div className="header">What To Do In Denver</div>
             <div className="underHeader">
-                /* <img src={denver} alt="denver" className="backgroundImg"> */
-                    <button className="questionnaireBtn">Start the questionnaire</button>
-                </img>
+                <img src={rmnp} alt="denver" className="backgroundImg" />
+                <button className="questionnaireBtn">Start the questionnaire</button>
             </div>
         </div>
     )

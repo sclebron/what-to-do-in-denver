09d48abdc034587b46d33a7960ee46efdf2c9a8d
SLeBron <sophieclebron@gmail.com>
2023-06-08 22:53:54 -0600

    imported use navigate hook into questionnaire.js

--- src/pages/questionnaire/questionnaire.js
+++ src/pages/questionnaire/questionnaire.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './questionnaire.css';
 
 function Questionnaire() {
+    const navigate = useNavigate();
+
+
     return (
         <div className="questionnaireContainer">
             

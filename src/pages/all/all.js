de1eb2b36bae2a9ef263ac7a44b3f1d91b569869
SLeBron <sophieclebron@gmail.com>
2023-08-17 14:11:31 -0600

    added comment

--- src/pages/all/all.js
+++ src/pages/all/all.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './all.css';
 
+//have json of activities here -> if activity has thing that was selected included then add it to empty array(?) of activities that will be displayed
+
 function All() {
     return (
         <div className="allContainer">

e08f9c44cf5f3ae6601a92b4ee0e3dd7afba0c0e
SLeBron <sophieclebron@gmail.com>
2023-08-21 12:38:21 -0600

    mapped through random activities in random.js

--- src/pages/random/random.js
+++ src/pages/random/random.js
@@ -21,9 +21,15 @@ function Random() {
         <div className="random">
         <div className="randomContainer">
             <div className="randomTitle">{random.title}</div>
-            <img className="randomImg" src={random.image} alt="Random activity" />
-            <div className="randomDescription">{random.description}</div>
-            <div className="randomWebUrl">{random.webUrl}</div>
+            <div className="randomActivity">
+            {random.map((random) => (
+                <>
+                <img className="randomImg" src={random.image} alt="Random activity" />
+                <div className="randomDescription">{random.description}</div>
+                <div className="randomWebUrl">{random.webUrl}</div>
+                </>
+            ))}
+            </div>
         </div>
         </div>
     )

0f5a0250508a7485328e8db13763c58c585bec9a
SLeBron <sophieclebron@gmail.com>
2023-10-06 16:15:17 -0600

    updated all.js

--- src/pages/all/all.js
+++ src/pages/all/all.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './all.css';
 
-function All({allActivities}) {
+function All(props) {
+
+    const allActivities = props.allActivities;
 
     return (
         <div className="all">

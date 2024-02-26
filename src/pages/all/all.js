2015cf3a7c60b301e1c9d666e55c145514b37a07
SLeBron <sophieclebron@gmail.com>
2024-02-26 15:01:53 -0700

    changed li to div

--- src/pages/all/all.js
+++ src/pages/all/all.js
@@ -16,14 +16,14 @@ function All() {
         <div className="all">
         <div className="allContainer">
         {allActivities?.map((activity) => (
-                <li key={activity.id}>
+                <div key={activity.id}>
                 <div className="allTitle">{activity.title}</div>
                 <img className="allImg" src={activity.image} alt="All activity" />
                 <div className="allActivity">
                     <div className="allDescription">{activity.description}</div>
                     <a className="allWebUrl" href={activity.webUrl} target="blank">Visit Website</a>
                 </div>
-                </li>
+                </div>
             ))}
         </div>
         </div>

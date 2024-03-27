59fff544f31bb82c628b9ab07f06a39f6df32255
SLeBron <sophieclebron@gmail.com>
2024-03-27 13:29:41 -0600

    wrote cypress tests to check if elements on homepage are displayed

--- cypress/e2e/wtdid.cy.js
+++ cypress/e2e/wtdid.cy.js
@@ -5,5 +5,22 @@ describe('what-to-do-in-denver', () => {
         cy.visit('https://sclebron.github.io/what-to-do-in-denver/')
     })
 
-    
+    //should have backgroundimage
+    it('displays background image', () => {
+        cy.get('')
+    })
+
+    //should display header
+    it('displays header on homepage', () => {
+        cy.get('.header').should('be.visible')
+    })
+
+    //should display questionnaire button
+    it('displays questionnaire button', () => {
+        cy.get('.questionnaireBtn').should('be.visible');
+    })
+
+    //questionnaire button should redirect to questionnaire page
+
+
 })

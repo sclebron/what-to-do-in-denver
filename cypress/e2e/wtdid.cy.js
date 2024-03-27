da7f60e4b8c39e5e8ba908774639c9280d09a9c1
SLeBron <sophieclebron@gmail.com>
2024-03-27 13:05:51 -0600

    started e2e cypress tests

---
+++ cypress/e2e/wtdid.cy.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+describe('what-to-do-in-denver', () => {
+    beforeEach(() => {
+        cy.visit('https://sclebron.github.io/what-to-do-in-denver/')
+    })
+
+    
+})

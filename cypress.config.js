ae79314c322c2a84d7cf9c97b42360e6e4ed1d47
SLeBron <sophieclebron@gmail.com>
2024-05-01 10:28:00 -0600

    changed baseUrl and cy.visit urls to try to fix 404 error for questionnaire page tests

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://sclebron.github.io/what-to-do-in-denver/',
+    baseUrl: 'https://sclebron.github.io',
   },
   component: {
     devServer: {
